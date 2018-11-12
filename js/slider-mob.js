a992c34d6ecfffadd77a4957ef3255cec306b964
Zi-web <37513694+Zi-web@users.noreply.github.com>
2018-11-12 21:30:54 +0300

    Add files via upload

---
+++ js/slider-mob.js
@@ -0,0 +1,22 @@
+if(screen.width < 768) {
+ 
+var slideIndex = [1];
+var slideId = ["mySlides4"]
+showSlides(1, 3);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
+
+}
